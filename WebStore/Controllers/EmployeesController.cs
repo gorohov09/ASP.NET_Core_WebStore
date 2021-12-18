5c35807ddc4e9b597c33213c1ee7da643e86867c
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-12-18 14:17:12 +0300

    Вынесение сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -6,15 +7,12 @@ namespace WebStore.Controllers
     //[Route("Staff/{action=Index}/{id?}")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> List_Employees = new List<Employee>()
-        {
-            new Employee() {Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27, Birthday = new DateTime(1996, 12 ,2), Salary = 50000},
-            new Employee() {Id = 2, LastName = "Максимов", FirstName = "Максим", Patronymic = "Максимович", Age = 28, Birthday = new DateTime(1997, 4, 1), Salary = 60000},
-            new Employee() {Id = 3, LastName = "Сергеев", FirstName = "Сергей", Patronymic = "Сергеевич", Age = 24, Birthday = new DateTime(1994, 8, 4), Salary = 70000},
-            new Employee() {Id = 4, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 20, Birthday = new DateTime(1999, 6, 15), Salary = 30000},
-            new Employee() {Id = 5, LastName = "Андреев", FirstName = "Андрей", Patronymic = "Андреевич", Age = 21, Birthday = new DateTime(2000, 10, 14), Salary = 45000},
-        };
+        private ICollection<Employee> List_Employees;
 
+        public EmployeesController()
+        {
+            List_Employees = TestData.Employees;
+        }
         public IActionResult Index()
         {
             var result = List_Employees;
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,16 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get;  } = new List<Employee>()
+        {
+            new Employee() {Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27, Birthday = new DateTime(1996, 12 ,2), Salary = 50000},
+            new Employee() {Id = 2, LastName = "Максимов", FirstName = "Максим", Patronymic = "Максимович", Age = 28, Birthday = new DateTime(1997, 4, 1), Salary = 60000},
+            new Employee() {Id = 3, LastName = "Сергеев", FirstName = "Сергей", Patronymic = "Сергеевич", Age = 24, Birthday = new DateTime(1994, 8, 4), Salary = 70000},
+            new Employee() {Id = 4, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 20, Birthday = new DateTime(1999, 6, 15), Salary = 30000},
+            new Employee() {Id = 5, LastName = "Андреев", FirstName = "Андрей", Patronymic = "Андреевич", Age = 21, Birthday = new DateTime(2000, 10, 14), Salary = 45000},
+        };
+    }
+}
