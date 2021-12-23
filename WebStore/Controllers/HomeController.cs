c4d74fb8dbad32e778377bf57fca7ee972767174
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-12-23 17:52:38 +0300

    Каталог товаров

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index([FromServices]IProductData ProductData)
         {
+            var products = ProductData.GetProducts()
+                .OrderBy(p => p.Order)
+                .Take(6)
+                .Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                });
+
+            ViewBag.Products = products;
+
             return View();
         }
 
