29e8520c1b486114184d86969447149a56a94e17
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-12-25 01:45:59 +0300

    ViewSection и визуальные компоненты

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public SectionsViewComponent(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
