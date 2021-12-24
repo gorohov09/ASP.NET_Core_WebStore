565ec295c36501d333b81f295e9403a89c3c7f91
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-12-25 00:54:35 +0300

    Валидация данных

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -1,18 +1,19 @@
-﻿namespace WebStore.Models
+﻿
+namespace WebStore.Models
 {
     public class Employee
     {
         public int Id { get; set; } 
         
-        public string LastName { get; set; }    
+        public string LastName { get; set; }
 
-        public string FirstName { get; set; }   
+        public string FirstName { get; set; }
 
         public string Patronymic { get; set; }
-        
+
         public int Age { get; set; }
 
-        public DateTime Birthday { get; set; }  
+        public DateTime Birthday { get; set; }
 
         public int Salary { get; set; } 
     }
--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,19 +1,38 @@
-﻿namespace WebStore.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput (DisplayValue = false)]
         public int Id { get; set; }
 
+        [Display (Name = "Фамилия")]
+        [Required (ErrorMessage = "Фамилия обязательна")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Длина должна быть от 2 до 255")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата")]
         public string LastName { get; set; }
 
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Имя обязательно")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Длина должна быть от 2 до 255")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Отчество")]
+        [StringLength(255, ErrorMessage = "Длина должна быть до 255")]
+        [RegularExpression(@"(([А-ЯЁ][а-яё]+)|([A-Z][a-z]+))?", ErrorMessage = "Ошибка формата")]
         public string Patronymic { get; set; }
 
-        public int Age { get; set; } 
-        
+        [Display(Name = "Возраст")]
+        [Range(18, 80, ErrorMessage = "Возраст должен быть от 18 до 80 лет")]
+        public int Age { get; set; }
+
+        [Display(Name = "Дата рождения")]
         public DateTime Birthday { get; set; }
 
+        [Display(Name = "Зарплата")]
         public int Salary { get; set; }
     }
 }
