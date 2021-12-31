174cacb46fbe35b10a50d295595bdcc3f6aa3900
Горохов Андрей <AnSGorokhov@kzn.beeline.ru>
2021-12-31 18:43:50 +0500

    Реализация сервиса каталога товаров в БД

---
+++ WebStore/Services/InSQL/SqlProductData.cs
@@ -0,0 +1,40 @@
+﻿using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services.InSQL
+{
+    public class SqlProductData : IProductData
+    {
+        private readonly WebStoreDB _db;
+
+        public SqlProductData(WebStoreDB db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _db.Brands;
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+        {
+            IQueryable<Product> query = _db.Products;
+
+            if (Filter?.SectionId != null)
+                query = query.Where(p => p.SectionId == Filter.SectionId);
+            if (Filter?.BrandId != null)
+                query = query.Where(p => p.BrandId == Filter.BrandId);
+
+            return query;
+
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return _db.Sections;
+        }
+    }
+}
