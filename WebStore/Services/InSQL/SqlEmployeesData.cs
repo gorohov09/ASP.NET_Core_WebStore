98649cf3a54f3fbe6934162ea03cde945ea3882a
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-01-05 20:54:28 +0500

    Улучшение работы с сотрудниками

--- WebStore/Services/InSQL/SqlEmployeesData.cs
+++ WebStore/Services/InSQL/SqlEmployeesData.cs
@@ -8,9 +8,12 @@ namespace WebStore.Services.InSQL
     {
         private readonly WebStoreDB _db;
 
-        public SqlEmployeesData(WebStoreDB db)
+        private readonly ILogger<SqlEmployeesData> _Logger;
+
+        public SqlEmployeesData(WebStoreDB db, ILogger<SqlEmployeesData> Logger)
         {
             _db = db;
+            _Logger = Logger;
         }
 
         public int Add(Employee employee)
@@ -22,32 +25,43 @@ namespace WebStore.Services.InSQL
                 return employee.Id;
 
             _db.Employees.Add(employee);
-            _db.SaveChanges();
+
+            _db.SaveChanges(); //только здесь Объект(employee) получит свойство Id
 
             return employee.Id;
         }
 
         public bool Delete(int id)
         {
-            var employee = GetById(id);
+            //var employee = GetById(id);
+            var employee = _db.Employees.Select(e => new Employee()
+            {
+                Id = e.Id
+            }).FirstOrDefault(e => e.Id == id); //Экономия памяти(Удаляем по Id)
+
             if (employee is null)
             {
+                _Logger.LogWarning("Попытка удаления отсутсвующего сотрудника с Id:{0}", employee.Id);
                 return false;
             }
 
-            _db.Remove(employee);
+            _db.Employees.Remove(employee);
+
             _db.SaveChanges();
+
+            _Logger.LogInformation("Сотруднике с id:{0} был успешно удален", employee.Id);
 
             return true;
         }
 
         public bool Edit(Employee employee)
         {
+            #region 1_вариант
             //if (employee == null)
             //    throw new ArgumentException(nameof(employee));
 
-            //if (_Employees.Contains(employee))
-            //    return true;
+            ////if (_db.Employees.Contains(employee))
+            ////    return true;
 
             //var db_employee = GetById(employee.Id);
 
@@ -65,9 +79,16 @@ namespace WebStore.Services.InSQL
             //db_employee.Birthday = employee.Birthday;
             //db_employee.Salary = employee.Salary;
 
+            //_db.SaveChanges();
+
             //_Logger.LogInformation("Информация о сотруднике id:{0} была изменена", employee.Id);
 
-            return true;
+            //return true;
+            #endregion
+
+            _db.Employees.Update(employee);
+
+            return _db.SaveChanges() != 0;
         }
 
         public IEnumerable<Employee> GetAll()
@@ -77,7 +98,8 @@ namespace WebStore.Services.InSQL
 
         public Employee? GetById(int id)
         {
-            return _db.Employees.FirstOrDefault(emp => emp.Id == id);
+            //return _db.Employees.FirstOrDefault(emp => emp.Id == id);
+            return _db.Employees.Find(id); //Берем из кэша
         }
     }
 }
