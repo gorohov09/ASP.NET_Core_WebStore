1e386ff993f1134e0ebb6bee54ca0cbe9c6fe0b6
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-01-15 12:07:13 +0300

    Логика процесса регистрации

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,16 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register()
+        private readonly UserManager<User> _userManager;
+
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
         {
-            return View();
+            _userManager = UserManager;
+            _signInManager = SignInManager;
         }
 
-        public IActionResult Login()
+        [HttpGet]
+        public IActionResult Register()
         {
+            RegisterUserViewModel registerUserView = new RegisterUserViewModel();
+
+            return View(registerUserView);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new User()
+            {
+                UserName = model.UserName,
+            };
+
+            var registration_result = await _userManager.CreateAsync(user);
+
+            if (registration_result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+
+                RedirectToAction("Home", "Index");
+            }
+
+            foreach (var error in registration_result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Login()
+        {
+
             return View();
         }
 
