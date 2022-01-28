8b51c1f799d8ca328a01ad8883cc6bf1c5923f74
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-01-28 23:50:37 +0300

    Контроллер корзины

--- WebStore/Controllers/CartController.cs
+++ WebStore/Controllers/CartController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService)
         {
-            return View();
+            _CartService = CartService;
+        }
+
+        public IActionResult Index() => View(_CartService.GetViewModel());
+
+        public IActionResult Add(int Id)
+        {
+            _CartService.Add(Id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Decrement(int Id)
+        {
+            _CartService.Decrement(Id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Remove(int Id)
+        {
+            _CartService.Remove(Id);
+            return RedirectToAction("Index", "Cart");
         }
     }
 }
