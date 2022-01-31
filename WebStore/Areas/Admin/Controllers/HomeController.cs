8ac88967c6181c449a4ef3273de2f9dc7fee6f3f
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-01-31 22:17:45 +0300

    Подготовка инфраструктуры для области администратора

---
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")] //Указываем, что данный контроллер находится в области администратора
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
