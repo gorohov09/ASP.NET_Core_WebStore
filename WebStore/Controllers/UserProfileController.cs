1ac546891f7b12920528666440f4f212b0e62d81
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-05 13:30:48 +0300

    Профиль пользователя с отображением в нем списка заказов

--- WebStore/Controllers/UserProfileController.cs
+++ WebStore/Controllers/UserProfileController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -7,6 +9,23 @@ namespace WebStore.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService OrderService)
+        {
+            var orders = await OrderService.GetUserOrdersAsync(User.Identity!.Name!);
+
+            var orders_model = orders.Select(o => new UserOrderViewModel()
+            {
+                Id = o.Id,
+                Address = o.Address,
+                Date = o.Date,
+                Description = o.Description,
+                Phone = o.Phone,
+                TotalPrice = o.TotalPrice,
+            });
+
+            return View(orders_model);
         }
     }
 }
