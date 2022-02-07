31e5852479b656900763d3865b4fee17e8ddb94d
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-07 12:57:34 +0300

    Создание таблицы товаров, работа в контроллере Products

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Mapping;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Role.Administrators)]
     public class ProductsController : Controller
     {
+        private readonly IProductData _ProductData;
+
+        private readonly ILogger<ProductsController> _Logger;
+
+        public ProductsController(IProductData productData, ILogger<ProductsController> logger)
+        {
+            _ProductData = productData;
+            _Logger = logger;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _ProductData.GetProducts().ToView();
+
+            return View(products);
         }
     }
 }
--- WebStore/ViewModels/ProductViewModel.cs
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -1,17 +1,24 @@
-﻿namespace WebStore.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
 {
     public class ProductViewModel
     {
         public int Id { get; set; }
 
+        [Display(Name = "Имя товара")]
         public string Name { get; set; }
 
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Картинка")]
         public string ImageUrl { get; set; }
 
+        [Display(Name = "Бренд")]
         public string? Brand { get; set; }
 
+        [Display(Name = "Категория")]
         public string Section { get; set; }
     }
 }
