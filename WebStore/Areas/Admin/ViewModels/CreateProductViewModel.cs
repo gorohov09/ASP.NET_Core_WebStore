9e5d185612f14b9678f7afef0363e3c2cd9930ce
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-08 11:33:19 +0300

    Мелкие правки

--- WebStore/Areas/Admin/ViewModels/CreateProductViewModel.cs
+++ WebStore/Areas/Admin/ViewModels/CreateProductViewModel.cs
@@ -1,17 +1,26 @@
-﻿namespace WebStore.Areas.Admin.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.Areas.Admin.ViewModels
 {
     public class CreateProductViewModel
     {
+        [Display(Name = "Имя")]
         public string Name { get; set; }
 
+        [HiddenInput]
         public int Order { get; set; }
 
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Картинка")]
         public string ImageUrl { get; set; }
 
+        [Display(Name = "Бренд")]
         public string Brand { get; set; }
 
+        [Display(Name = "Категория")]
         public string Section { get; set; }
     }
 }
--- WebStore/Areas/Admin/ViewModels/EditProductViewModel.cs
+++ WebStore/Areas/Admin/ViewModels/EditProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebStore.Areas.Admin.ViewModels
 {
@@ -7,6 +8,7 @@ namespace WebStore.Areas.Admin.ViewModels
         [HiddenInput]
         public int Id { get; set; }
 
+        [Display(Name = "Имя")]
         public string Name { get; set; }
 
         public int Order { get; set; }
@@ -14,15 +16,19 @@ namespace WebStore.Areas.Admin.ViewModels
         [HiddenInput]
         public int SectionId { get; set; }
 
+        [Display(Name = "Категория")]
         public string Section { get; set; }
 
         [HiddenInput]
         public int? BrandId { get; set; }
 
+        [Display(Name = "Бренд")]
         public string? Brand { get; set; }
 
+        [Display(Name = "Картинка")]
         public string ImageUrl { get; set; }
 
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
     }
 }
