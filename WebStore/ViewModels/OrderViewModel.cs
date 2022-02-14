9c22a61ad92f85127597f6078a6982abdfcf086f
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-02-14 17:33:52 +0300

    Реализация логики сервиса заказов

--- WebStore/ViewModels/OrderViewModel.cs
+++ WebStore/ViewModels/OrderViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace WebStore.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
 {
     public class OrderViewModel
     {
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
     }
 }
