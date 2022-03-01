ce59c37149a7f545e35ca0ae40650da964925aee
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-01 13:56:05 +0300

    Регистрация сервисов бизнес-логики в проекте хоста Web-API

--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
+using WebStore.Services.Services.InSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
@@ -5,6 +13,38 @@ var services = builder.Services;
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+services.AddDbContext<WebStoreDB>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))); //Добавление сервиса для работы с БД
+services.AddTransient<IDbInitializer, DbInitializer>(); //Добавление сервиса для инициализации БД
+
+services.AddIdentity<User, Role>() //Добавление системы Identity в наши сервисы
+    .AddEntityFrameworkStores<WebStoreDB>()
+    .AddDefaultTokenProviders();
+
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 3;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(20);
+
+});
+
+services.AddScoped<IEmployeesData, SqlEmployeesData>(); //Добавление нашего сервиса для работы с сотрудниками
+services.AddScoped<IProductData, SqlProductData>(); //Добавление сервиса для работы с продуктами
+services.AddScoped<IOrderService, SqlOrderService>();
 
 var app = builder.Build();
 
