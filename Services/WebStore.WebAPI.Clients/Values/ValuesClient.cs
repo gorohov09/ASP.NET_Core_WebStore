7aed4924d3587e25bd9370e9f55f495362a411a3
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-16 00:11:15 +0300

    Класс базового клиента WebAPI

---
+++ Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,37 @@
+﻿using WebStore.Interfaces;
+
+namespace WebStore.WebAPI.Clients.Values
+{
+    public class ValuesClient : IValueService
+    {
+        public void Add(string Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int Id, string Value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
