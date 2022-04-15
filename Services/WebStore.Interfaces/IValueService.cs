46d348ae26baf12bd8ae218d4e8d35e072e65f7a
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-16 00:04:59 +0300

    Создание интерфейса клиента

---
+++ Services/WebStore.Interfaces/IValueService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Interfaces
+{
+    public interface IValueService
+    {
+        IEnumerable<string> GetValues();
+
+        int Count();
+
+        string GetById(int Id);
+
+        void Add(string Value);
+
+        void Edit(int Id, string Value);
+
+        bool Delete(int Id);
+    }
+}
