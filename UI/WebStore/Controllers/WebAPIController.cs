420344163836a5f21aecfe0c6143edba58a92e95
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-16 14:41:51 +0300

    Визуальное управление WebAPI

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValueService _ValuesService;
+
+        public WebAPIController(IValueService ValueService)
+        {
+            _ValuesService = ValueService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _ValuesService.GetValues();
+
+            return View(values);
+        }
+    }
+}
