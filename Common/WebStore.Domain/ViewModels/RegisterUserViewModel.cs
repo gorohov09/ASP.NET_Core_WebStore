f9ba65fdfb3fe9483043599f2d00e8bf09bfe676
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-16 14:07:10 +0300

    Изменение ViewModel регистрации

--- Common/WebStore.Domain/ViewModels/RegisterUserViewModel.cs
+++ Common/WebStore.Domain/ViewModels/RegisterUserViewModel.cs
@@ -6,6 +6,25 @@ namespace WebStore.ViewModels
     {
         [Required]
         [Display(Name = "Имя пользователя")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия пользователя")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Дата рождения")]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [Display(Name = "Адрес электронной почты")]
+        public string Email { get; set; }
+
+        [Display(Name = "О себе")]
+        public string AboutMySelf { get; set; }
+
+        [Required]
+        [Display(Name = "Логин пользователя")]
         public string UserName { get; set; }
 
         [Required]
