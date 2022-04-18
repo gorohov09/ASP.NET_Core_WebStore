3a0d9d3b66ea9dadc877fcf88e3a40a3eb81d695
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-04-18 18:56:41 +0300

    Создание интерфейса IPersonService для клиента

---
+++ Services/WebStore.Interfaces/Test/IPersonService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Interfaces
+{
+    public interface IPersonService
+    {
+        IEnumerable<Person> GetPersons();
+
+        int Count();
+
+        Person? GetById(int Id);
+
+        void Add(Person person);
+
+        bool Delete(int Id);
+    }
+}
