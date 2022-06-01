503c95cbd839a921fd6c461e7e888139fa066d45
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-01 22:22:15 +0300

    Редактирование метода обновления данных у сотрудника

--- Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -36,8 +36,8 @@ namespace WebStore.WebAPI.Controllers
         [HttpPut]
         public IActionResult Update(Employee employee)
         {
-            _EmployeesData.Edit(employee);
-            return Ok();
+            var success = _EmployeesData.Edit(employee);
+            return Ok(success);
         }
 
         [HttpDelete("{Id}")]
