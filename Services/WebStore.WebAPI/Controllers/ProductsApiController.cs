3d8cc8ff09200e27f3de8a4b52ae13c09bc76926
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-04 11:15:51 +0300

    Заготовка контроллера и клиента WebAPI для Products

---
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+
+    }
+}
