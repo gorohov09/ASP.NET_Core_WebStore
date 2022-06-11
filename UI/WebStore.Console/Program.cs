0964b135f7f3a63ccafe9e4500ca99efe124432a
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-06-11 19:52:08 +0300

    Пример использования http-клиента в консольном приложении

---
+++ UI/WebStore.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Products;
+
+var service_collection = new ServiceCollection();
+service_collection.AddHttpClient<IProductData, ProductsClient>(http => http.BaseAddress = new("http://localhost:5036"));
+
+var provider = service_collection.BuildServiceProvider();
+
+Console.WriteLine("Ожидание старта хоста WebAPI");
+Console.ReadLine();
+
+var product_data = provider.GetRequiredService<IProductData>();
+
+var products = product_data.GetProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine($"{product.Id}-{product.Name}-{product.Price}-{product.Section}-{product.Brand}");
+}
