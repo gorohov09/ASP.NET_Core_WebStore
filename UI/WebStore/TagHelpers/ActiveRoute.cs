396e5cca5440b034b98efadde4e8c2c8a8c6cfb5
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-22 18:18:51 +0500

    Логика тэг-хелпера подсветки активной ссылки в главном меню

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -7,7 +7,8 @@ namespace WebStore.TagHelpers
     [HtmlTargetElement(Attributes = AttributeName)]
     public class ActiveRoute : TagHelper
     {
-        private const string AttributeName = "is-active-route";
+        private const string AttributeName = "ws-is-active-route";
+        private const string IgnoreAction = "ws-ignore-action";
 
         [HtmlAttributeName("asp-controller")]
         public string Controller { get; set; }
@@ -24,8 +25,43 @@ namespace WebStore.TagHelpers
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.Attributes.RemoveAll(AttributeName);
+            var is_ignore_action = output.Attributes.RemoveAll(IgnoreAction);
 
+            if (IsActive(is_ignore_action))
+                MakeAction(output);
+        }
 
+        private void MakeAction(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value?.ToString().Contains("active") ?? false)
+                    return;
+                output.Attributes.SetAttribute("class", $"{class_attribute.Value} active");
+            }
+        }
+
+        private bool IsActive(bool isIgnoreAction)
+        {
+            var route_values = ViewContext.RouteData.Values;
+
+            var route_controller = route_values["controller"]?.ToString();
+            var route_action = route_values["action"]?.ToString();
+
+            if (!isIgnoreAction && Action is { Length: > 0 } action && !string.Equals(action, route_action))
+                return false;
+            if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key]?.ToString() != value)
+                    return false;
+
+            return true;
         }
     }
 }
