6c01971a95caeea39739f0f18761c0843350ad2e
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-22 23:30:20 +0500

    Логика формирования хлебных крошек

--- Common/WebStore.Domain/ViewModels/BreadCrumbsViewModel.cs
+++ Common/WebStore.Domain/ViewModels/BreadCrumbsViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace WebStore.Domain.ViewModels
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.ViewModels
 {
     public class BreadCrumbsViewModel
     {
+        public Section? Section { get; set; }
+
+        public Brand? Brand { get; set; }
+
+        public string ProductName { get; set; }
     }
 }
