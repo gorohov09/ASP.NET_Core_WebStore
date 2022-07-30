aa471c51b68b5784e04f02818b28ee3dede947de
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-30 14:20:37 +0500

    Создание сущности - Блог

---
+++ Common/WebStore.Domain/Entities/Blog.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities;
+
+[Index(nameof(Title), IsUnique = true)]
+public class Blog : Entity
+{
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public DateTime Date { get; set; }
+
+    public int? Stars { get; set; }
+
+    public string ImageUrl { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
